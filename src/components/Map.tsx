b7a9c450a01c192162ab2bfa86922c8f6611bfa8
alireza <alirezafrance2024@gmail.com>
2025-12-02 19:02:09 +0000

    بازم نشد

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,52 +1,37 @@
+
 'use client';
 
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { useEffect, useRef } from 'react';
 
 // This is to fix the default icon issue with webpack
-// @ts-ignore
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: '/images/marker-icon-2x.png',
-    iconUrl: '/images/marker-icon.png',
-    shadowUrl: '/images/marker-shadow.png',
+const icon = L.icon({
+  iconUrl: '/images/marker-icon.png',
+  iconRetinaUrl: '/images/marker-icon-2x.png',
+  shadowUrl: '/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
 });
 
+const position: L.LatLngExpression = [29.6393, 52.5155]; // Shiraz, Mosalla Nezhad St
 
-const MapComponent = () => {
-  const mapContainerRef = useRef<HTMLDivElement | null>(null);
-  const mapInstanceRef = useRef<L.Map | null>(null);
-  const position: L.LatLngExpression = [29.6393, 52.5155]; // Shiraz, Mosalla Nezhad St
-
-  useEffect(() => {
-    // Initialize map only if the container exists and map instance is not already created.
-    if (mapContainerRef.current && !mapInstanceRef.current) {
-      const map = L.map(mapContainerRef.current).setView(position, 16);
-      mapInstanceRef.current = map; // Store instance
-
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
-
-      L.marker(position).addTo(map)
-        .bindPopup('دکه ابوعلی <br /> خیابان مصلی نژاد');
-    }
-
-    // Cleanup function to run when the component unmounts
-    return () => {
-      if (mapInstanceRef.current) {
-        mapInstanceRef.current.remove();
-        mapInstanceRef.current = null;
-      }
-    };
-  }, []); // Empty dependency array ensures this runs only once on mount
-
-
+const Map = () => {
   return (
-    <div ref={mapContainerRef} className="h-full w-full" />
+    <MapContainer center={position} zoom={16} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position} icon={icon}>
+        <Popup>
+          دکه ابوعلی <br /> خیابان مصلی نژاد
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
-export default MapComponent;
+export default Map;
