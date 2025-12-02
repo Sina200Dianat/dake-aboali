ff99472d045b4532d9749e211b3fe4697f89acae
alireza <alirezafrance2024@gmail.com>
2025-12-02 14:06:53 +0000

    Try fixing this error: `Runtime Error: Error: Map container is already i

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,22 +4,26 @@
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
-
-// This is to fix the default icon issue with webpack
-// It's a workaround for a known issue with react-leaflet and Next.js
-if (typeof window !== 'undefined') {
-  delete (L.Icon.Default.prototype as any)._getIconUrl;
-
-  L.Icon.Default.mergeOptions({
-      iconRetinaUrl: '/images/marker-icon-2x.png',
-      iconUrl: '/images/marker-icon.png',
-      shadowUrl: '/images/marker-shadow.png',
-  });
-}
-
+import { useEffect } from 'react';
 
 const MapComponent = () => {
   const position: L.LatLngExpression = [29.6393, 52.5155]; // Shiraz, Mosalla Nezhad St
+
+  useEffect(() => {
+    // This is to fix the default icon issue with webpack
+    // It's a workaround for a known issue with react-leaflet and Next.js
+    // We do it in useEffect to ensure it only runs on the client
+    if (typeof window !== 'undefined') {
+        // @ts-ignore
+        delete L.Icon.Default.prototype._getIconUrl;
+
+        L.Icon.Default.mergeOptions({
+            iconRetinaUrl: '/images/marker-icon-2x.png',
+            iconUrl: '/images/marker-icon.png',
+            shadowUrl: '/images/marker-shadow.png',
+        });
+    }
+  }, []);
 
   return (
     <MapContainer center={position} zoom={16} scrollWheelZoom={false} className="h-full w-full">
